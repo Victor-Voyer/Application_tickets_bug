3ba8cf7885f8e835cc28256fbc5e58145d32c55d
Victor-Voyer <victor.voyer@oclock.school>
2025-09-29 14:29:39 +0200

    create crud for UsersController

---
+++ src/Form/UsersType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Roles;
+use App\Entity\Users;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nickname')
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('password')
+            ->add('role', EntityType::class, [
+                'class' => Roles::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
