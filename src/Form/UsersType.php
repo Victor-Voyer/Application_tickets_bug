ecf8bd0e2cd19c8057e7e8512c9ba242e1a9245d
Victor-Voyer <victor.voyer@oclock.school>
2025-09-30 10:00:47 +0200

    update UsersType password

--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -2,10 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Roles;
 use App\Entity\Users;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,11 +17,9 @@ class UsersType extends AbstractType
             ->add('first_name')
             ->add('last_name')
             ->add('email')
-            ->add('password')
-            ->add('role', EntityType::class, [
-                'class' => Roles::class,
-                'choice_label' => 'id',
-            ])
+            ->add('password', PasswordType::class)
+            
+            
         ;
     }
 
