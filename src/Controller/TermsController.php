93cac8521a7a375e3a8bfcdf0966153708f3df62
Victor-Voyer <victor.voyer@oclock.school>
2025-10-08 16:23:53 +0200

    create : ToU_view and link in footer

---
+++ src/Controller/TermsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TermsController extends AbstractController
+{
+    #[Route('/terms-of-use', name: 'app_terms')]
+    public function index(): Response
+    {
+        return $this->render('terms.html.twig');
+    }
+}
+
